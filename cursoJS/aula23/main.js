d26d13b6f4b0fb2eb908a074b01ccea78e338a07
JoasGomes <jojoca.vitor777@gmail.com>
2022-03-28 13:06:43 -0300

    aula23

---
+++ cursoJS/aula23/main.js
@@ -0,0 +1,73 @@
+//const video1 = new Promise ((resolve,reject)=>{
+//    console.log('iniciando o video 1')
+//    setTimeout(()=>{
+//        resolve('video1 está pronto')
+//    },2000)
+//});
+
+//const video2 = new Promise ((resolve,reject)=>{
+//   resolve('video2 está pronto')
+//})
+
+//const video3 = new Promise ((resolve,reject)=>{
+//    resolve('video3 está pronto')
+//})
+
+//Promise.all([video1,video2,video3]).then((results)=>{
+//    console.log(results)
+//})
+
+
+/* const request = obj =>{
+    const xhr = new XMLHttpRequest();
+    xhr.open(obj.method, obj.url, true)
+    xhr.send()
+
+    xhr.addEventListener('load',()=>{
+        if(xhr.status >= 200 && xhr.status < 300){
+            obj.success(xhr.responseText)
+        } else{
+            obj.error({
+                code: xhr.status,
+                msg: xhr.statusText
+            })
+        }
+    })
+} */
+
+document.addEventListener('click', e=>{
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+
+    if(tag === 'a'){
+        e.preventDefault();
+        carregaPagina(el)
+    }
+})
+
+async function carregaPagina(el){
+    try{
+        const href = el.getAttribute('href');
+    const response = await fetch(href)
+
+    if(response.status !== 200) throw new Error('erro 404')
+
+    const html = await response.text()
+    carregaResultado(html)
+    } catch(e){
+        console.log(e)
+    }
+}
+function carregaResultado(response){
+    const resultado = document.querySelector('.resultado')
+    resultado.innerHTML = response
+}
+
+
+
+
+
+
+
+
+
