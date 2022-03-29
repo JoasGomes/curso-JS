18322b7956bebe9edce5bb2da653144a5f6e846c
JoasGomes <jojoca.vitor777@gmail.com>
2022-03-29 20:38:50 -0300

    anotações nodejs

---
+++ cursoJS/nodeMasterclass/main.js
@@ -0,0 +1,17 @@
+// oq é preciso saber...
+/*
+1- fundamentos de js (objetos,vetores,condicionais,funções)
+2- https,json,callbacks,promisses,arrow functions,es6
+*/
+
+/*
+- oq é ?
+r: é um js runtime enviroment
+- ele não é um framework nem uma linguagem
+- ecossistema gigante (NPM)
+- trabalha com cmmon js
+- 
+*/
+
+//const os = require('os')
+//console.log(os.platform())
